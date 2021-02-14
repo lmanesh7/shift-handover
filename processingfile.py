1baca384152fbac0ecce5c950e9b40954cdc480a
lmanesh7 <47899760+lmanesh7@users.noreply.github.com>
2021-02-14 19:56:41 +0530

    Update processingfile.py

--- processingfile.py
+++ processingfile.py
@@ -103,7 +103,7 @@ if file is not None:
 
         # Upload file for testing
         folder_path = st.text_input('Enter directory: default .', '.')
-        filename = "processed file.csv"
+        filename = "df"
 
         # Load selected file
         with open(filename, 'rb') as f:
