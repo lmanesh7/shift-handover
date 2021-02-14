48ea568680b8c34e05736e27e9bc5da36d3baa95
lmanesh7 <47899760+lmanesh7@users.noreply.github.com>
2021-02-14 20:34:02 +0530

    Update processingfile.py

--- processingfile.py
+++ processingfile.py
@@ -85,7 +85,7 @@ if file is not None:
         else:
             a[i] = ">3days"
     df['Age>3'] = a
-    df.to_csv("https://raw.githubusercontent.com/lmanesh7/shift-handover/main/processed%20file.csv")
+    df.to_csv("lmanesh7/shift-handover/main/processed file.csv")
     
     if st.checkbox('Select a file to download'):
         
